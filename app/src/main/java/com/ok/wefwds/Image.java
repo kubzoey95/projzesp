d0733cde3642ff7a28e7b234f9756047282ca893
kubzoey95 <jakubsliwice@o2.pl>
2018-11-06 22:29:16 +0100

    obrazki w dobrej jakosci + pliki zwiazane z git/OpencvSDK

--- app/src/main/java/com/ok/wefwds/Image.java
+++ app/src/main/java/com/ok/wefwds/Image.java
@@ -1,6 +1,10 @@
 package com.ok.wefwds;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -8,11 +12,19 @@ import org.opencv.android.Utils;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import static java.lang.Math.abs;
 import static org.opencv.core.CvType.CV_64FC1;
 
 /**
  * Created by kubzoey on 2018-10-23.
+ *
+ * Image handling and processing.
  */
 
 public class Image {
@@ -27,16 +39,33 @@ public class Image {
         imageMatrix = mat;
     }
 
+
     public Image(Camera cam){
         Bitmap bmp = cam.getBitmap();
         imageMatrix = Mat.zeros(bmp.getHeight(), bmp.getWidth(), CV_64FC1);
-        Utils.bitmapToMat(cam.getBitmap(), imageMatrix);
+        Utils.bitmapToMat(bmp, imageMatrix);
+    }
+
+    public Image(Uri uri){
+        Bitmap bmp = BitmapFactory.decodeFile(uri.getPath());
+        imageMatrix = Mat.zeros(bmp.getHeight(), bmp.getWidth(), CV_64FC1);
+        Utils.bitmapToMat(bmp, imageMatrix);
     }
 
     public Bitmap getBitmap(){
         Bitmap bmp = Bitmap.createBitmap(imageMatrix.width(), imageMatrix.height(), Bitmap.Config.RGB_565);
         Utils.matToBitmap(imageMatrix, bmp);
         return bmp;
+    }
+
+    static public File createImageFile(Context c) throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        File storageDir = c.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".bmp", storageDir);
+
+        return image;
     }
 
     public void makeGray(){
@@ -52,15 +81,7 @@ public class Image {
     }
 
     public void makeBinary(){
-        int avg = highestContrastAverage();
-        if (avg > 127){
-            //Imgproc.threshold(imageMatrix,imageMatrix, highestContrastAverage(), 255, Imgproc.THRESH_BINARY_INV);
-            Imgproc.adaptiveThreshold(imageMatrix, imageMatrix, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 11, 2);
-        }
-        else {
-            //Imgproc.threshold(imageMatrix,imageMatrix, highestContrastAverage(), 255, Imgproc.THRESH_BINARY_INV);
-            Imgproc.adaptiveThreshold(imageMatrix, imageMatrix, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 11, 2);
-        }
+        Imgproc.adaptiveThreshold(imageMatrix, imageMatrix, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 51, 2);
     }
 
     public int averageColor(){
